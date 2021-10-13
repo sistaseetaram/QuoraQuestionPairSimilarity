f5d40ec4b9f1f9a1138bfd63d4bf02cc5243fc4c
Sista Seetaram <hanumaseetaram@gmail.com>
2021-10-13 23:50:35 +0530

    update index.py

--- index.py
+++ index.py
@@ -6,17 +6,26 @@ import os
 import pickle as pk
 from urllib.request import urlopen
 # %%
-DATA_URL = ('')
-
+# DATA_URL = ('E:/')
+os.chdir('E:/Projects/QuoraQuestionPairSimilarity/')
 # %%
-@st.cache
+# @st.cache
 def load_data():
-    model = pk.load(urlopen(DATA_URL, 'rb'))
-    return model 
+    # model = pk.load(urlopen(DATA_URL, 'rb'))    
+    df=pd.read_pickle("df_preprocessed.pkl")
+    with open('Stack.pkl','rb') as pk_file:
+        model = pk.load(pk_file)
+    return df,model 
 
 data_load_state =st.text('Loading Required dependencies...')
 
-model = load_data()
+df,model = load_data()
 
 data_load_state.text("Done!")
 
+# %%
+st.header("Quora Question Pair Similarity Prototype")
+ques = st.text_area("",value= "Ask Question here...")
+
+st.info('This is a purely informational message')
+st.success('Similar question(s) Found!!')
